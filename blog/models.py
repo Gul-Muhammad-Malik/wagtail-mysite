2d91af485186dfccaacafa658386081408c1d6b6
Gul Muhammad <gulmuhammad.trader@gmail.com>
2026-02-19 08:09:31 +0500

    feat: Enhance BlogIndexPage context and update template to display blog posts

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,12 @@ from wagtail.fields import RichTextField
 class BlogIndexPage(Page):
     intro= RichTextField(blank=True)
     
+    def get_context(self, request):
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by("-first_published_at")
+        context["blogpages"] = blogpages
+        return context
+    
     content_panels = Page.content_panels + ["intro"]
     
 class BlogPage(Page):
